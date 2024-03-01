0faab6abf19163ed43aefa25810d465873471a73
quangnm-sonat <quangnm@sonat.vn>
2024-03-01 15:32:06 +0700

    feat: done auth

---
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
+import { Observable, map, tap } from 'rxjs';
+import { AUTH_SERVICE } from '../constants/service';
+import { ClientProxy } from '@nestjs/microservices';
+import { UserDto } from '../dto';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const jwt = request.cookies?.Authentication;
+    if (!jwt) {
+      return false;
+    }
+    return this.authClient
+        .send<UserDto>('authenticate', {
+          Authentication: jwt,
+        })
+        .pipe(
+          tap((res) => {
+            context.switchToHttp().getRequest().user = res;
+          }),
+      map(() => true)
+    );
+  }
+}
