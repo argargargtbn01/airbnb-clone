ae492eacc9f277598b2ab1fdddfd8c31f2f6d930
quangnm-sonat <quangnm@sonat.vn>
2024-03-14 16:32:51 +0700

    feat: doing deploy seperate libs for seperate services

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { DatabaseModule } from '@app/common/database/database.module';
-import { UserDocument, UserSchema } from './models/users.schema';
 import { LoggerModule } from '@app/common/logger/logger.module';
 import { UsersRepository } from './users.repository';
+import { UserDocument, UserSchema } from '@app/common';
 
 @Module({
   imports: [
