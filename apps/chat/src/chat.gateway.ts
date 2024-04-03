c52d30a9f301f7b40c6d76be6fc84c79e7055f39
quangnm-sonat <quangnm@sonat.vn>
2024-04-03 10:22:59 +0700

    fix: doing chat

--- apps/chat/src/chat.gateway.ts
+++ apps/chat/src/chat.gateway.ts
@@ -1,36 +1,47 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { OnGatewayConnection } from '@nestjs/websockets';
 
 @WebSocketGateway()
 export class ChatGateway {
   @WebSocketServer()
   server: Server;
 
-  onModuleInit(){}
-  
-  // @SubscribeMessage('message1')
-  // handleMessage(client: any, payload: any): string {
-  //   this.server.emit('a',{a:'b'})
-  //   return 'Hello world!';
-  // }
+  onModuleInit(client:Socket, ...args: any[]) {
+    console.log('dcm')
+    // console.log(client)
+  }
+
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(client)
+  }
+
+
 
   @SubscribeMessage('createRoom')
-  handleCreateRoom(client: Socket, room: string): void {
-    room='1'
-    client.join(room);
-    client.emit('roomCreated', room);
+  handleCreateRoom(@MessageBody() body, @ConnectedSocket() client: Socket): void {
+    console.log(body);
+    // room='1'
+    // client.join(room);
+    // client.emit('roomCreated', room);
   }
 
   @SubscribeMessage('joinRoom')
   handleJoinRoom(client: Socket, room: string): void {
-    room='1'
-
+    room = '1';
     client.join(room);
     client.emit('roomJoined', room);
   }
 
   @SubscribeMessage('message')
-  handleMessage(client: Socket, payload: { room: string, message: string }): void {
-    this.server.to('1').emit("message",payload.message);
+  handleMessage(client: Socket, payload: { room: string; message: string }): void {
+    this.server.to('1').emit('message', payload.message);
   }
 }
